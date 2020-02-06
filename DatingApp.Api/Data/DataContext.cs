dc6d44773457ba3e6f69dfa037f7799d5b5c1d8b
unknown <boudhraaalaa1@gmail.com>
2020-02-06 11:43:35 +0100

    First commit

---
+++ DatingApp.Api/Data/DataContext.cs
@@ -0,0 +1,11 @@
+using DatingApp.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.Api.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options):base (options) {}
+        public DbSet<Value> Values {get;set;}
+    }
+}
---
+++ Front/Dating-app/src/app/values/values.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-values',
+  templateUrl: './values.component.html',
+  styleUrls: ['./values.component.css']
+})
+export class ValuesComponent implements OnInit {
+values: any;
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.getValues();
+  }
+  getValues()
+  {
+    this.http.get('http://localhost:5000/api/values').subscribe(values => {
+    this.values=values;
+    console.log(values);
+    }),error=>{
+      console.log(error)  ;
+       }}
+
+}
